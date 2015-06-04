464742cdaf30ca8d33f473d84bf0050bc060947e
François Kooman <fkooman@tuxed.net>
2015-06-04 12:13:00 +0200

    update example

--- example/indieauth.php
+++ example/indieauth.php
@@ -23,71 +23,66 @@ use fkooman\Rest\Plugin\IndieAuth\IndieAuthAuthentication;
 use fkooman\Rest\Plugin\IndieAuth\IndieInfo;
 use GuzzleHttp\Client;
 
-try {
-    $service = new Service();
+$service = new Service();
 
-    // use discovery by default, fall back to IndieCert (https://indiecert.net/auth)
-    $indieAuth = new IndieAuthAuthentication();
+// use discovery by default, fall back to IndieCert (https://indiecert.net/auth)
+$indieAuth = new IndieAuthAuthentication();
 
-    // in case no session is active yet, redirect to '/'
-    $indieAuth->setUnauthorizedRedirectUri('/');
+// in case no session is active yet, redirect to '/'
+$indieAuth->setUnauthorizedRedirectUri('/');
 
-    $client = new Client(
-        array(
-            'defaults' => array(
-        // DO NOT set 'verify => false' in actual deployments!
-                'verify' => false,
-                'timeout' => 10,
-            ),
-        )
-    );
-    $indieAuth->setClient($client);
+$client = new Client(
+    array(
+        'defaults' => array(
+            // DO NOT set 'verify => false' in actual deployments!
+            'verify' => false,
+            'timeout' => 10,
+        ),
+    )
+);
+$indieAuth->setClient($client);
 
-    // use discovery by default, fall back to IndieAuth (https://indieauth.com/auth)
-    //$indieAuth = new IndieAuthAuthentication('https://indieauth.com/auth');
+// use discovery by default, fall back to IndieAuth (https://indieauth.com/auth)
+//$indieAuth = new IndieAuthAuthentication('https://indieauth.com/auth');
 
-    // disable discovery (i.e. "Distributed IndieAuth")
-    // $indieAuth->setDiscovery(false);
+// disable discovery (i.e. "Distributed IndieAuth")
+// $indieAuth->setDiscovery(false);
 
-    $service->registerOnMatchPlugin($indieAuth);
+$pluginRegistry = new PluginRegistry();
+$pluginRegistry->registerDefaultPlugin($indieAuth);
+$service->setPluginRegistry($pluginRegistry);
 
-    $service->get(
-        '/',
-        function (Request $request) {
-            // Show Sign In form;  POST to 'indieauth/auth' endpoint which is registered by the IndieAuth plugin
-            $output = '<html><head></style></head><body><h1>Sign In</h1>';
+$service->get(
+    '/',
+    function (Request $request) {
+        // Show Sign In form;  POST to 'indieauth/auth' endpoint which is registered by the IndieAuth plugin
+        $output = '<html><head></style></head><body><h1>Sign In</h1>';
 
-            // without requesting access token (no scope)
-            $output .= '<h2>Authentication Only</h2><form method="post" action="indieauth/auth">https://<input type="text" name="me" placeholder="example.org" required><input type="hidden" name="redirect_to" value="/success"><input type="submit" value="Sign In"></form>';
+        // without requesting access token (no scope)
+        $output .= '<h2>Authentication Only</h2><form method="post" action="indieauth/auth">https://<input type="text" name="me" placeholder="example.org" required><input type="hidden" name="redirect_to" value="/success"><input type="submit" value="Sign In"></form>';
 
-            // with requesting access token (scope)
-            $output .= '<h2>Authentication &amp; Token</h2><form method="post" action="indieauth/auth">https://<input type="text" name="me" placeholder="example.org" required><input type="text" name="scope" value="post"><input type="hidden" name="redirect_to" value="/success"><input type="submit" value="Sign In"></form>';
+        // with requesting access token (scope)
+        $output .= '<h2>Authentication &amp; Token</h2><form method="post" action="indieauth/auth">https://<input type="text" name="me" placeholder="example.org" required><input type="text" name="scope" value="post"><input type="hidden" name="redirect_to" value="/success"><input type="submit" value="Sign In"></form>';
 
-            $output .= '</body></html>';
+        $output .= '</body></html>';
 
-            return $output;
-        },
-        array(
-            // To view the index page, no authentication is required
-            'skipPlugins' => array(
-                'fkooman\Rest\Plugin\IndieAuth\IndieAuthAuthentication',
-             ),
-        )
-    );
+        return $output;
+    },
+    array(
+        // To view the index page, no authentication is required
+        'fkooman\Rest\Plugin\IndieAuth\IndieAuthAuthentication' => array('enabled' => false),
+    )
+);
 
-    // To view the "success" page, authentication is required
-    $service->get(
-        '/success',
-        function (IndieInfo $u) {
-            return sprintf(
-                '<html><head></head><body><h1>Hello</h1><table><tr><th>User ID</th><td>%s</td></tr><th>Access Token</th><td>%s</td></tr><tr><th>Scope</th><td>%s</td></tr></table><form method="post" action="indieauth/logout"><input type="submit" value="Logout"></form></body></html>',
-                $u->getUserId(), $u->getAccessToken(), $u->getScope()
-            );
-        }
-    );
+// To view the "success" page, authentication is required
+$service->get(
+    '/success',
+    function (IndieInfo $u) {
+        return sprintf(
+            '<html><head></head><body><h1>Hello</h1><table><tr><th>User ID</th><td>%s</td></tr><th>Access Token</th><td>%s</td></tr><tr><th>Scope</th><td>%s</td></tr></table><form method="post" action="indieauth/logout"><input type="submit" value="Logout"></form></body></html>',
+            $u->getUserId(), $u->getAccessToken(), $u->getScope()
+        );
+    }
+);
 
-    $service->run()->sendResponse();
-} catch (Exception $e) {
-    // in case en error occurred we display a HTML page with more details
-    Service::handleException($e)->sendResponse();
-}
+$service->run()->send();
