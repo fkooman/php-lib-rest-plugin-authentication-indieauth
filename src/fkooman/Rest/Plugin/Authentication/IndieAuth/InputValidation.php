16c4480b43e2f90aa5353deb95f82b126acd19e2
François Kooman <fkooman@tuxed.net>
2015-09-21 12:09:33 +0200

    source formatting

--- src/fkooman/Rest/Plugin/Authentication/IndieAuth/InputValidation.php
+++ src/fkooman/Rest/Plugin/Authentication/IndieAuth/InputValidation.php
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 namespace fkooman\Rest\Plugin\Authentication\IndieAuth;
 
 use fkooman\Http\Exception\BadRequestException;
